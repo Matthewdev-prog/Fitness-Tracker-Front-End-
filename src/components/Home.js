f0f4c83c6b28a6763a1bbdab6d95ef57c6b9fc53
zuly11 <zuly.rod.11@gmail.com>
2023-02-15 22:10:53 -0600

    modifited Home.js and added to Activities.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Home = () => {
-  return <>HOME</>;
+  return <>
+  
+  <div class="container">
+    <img  className="hero" src="../../assets/files/heropage.png" alt="homepage" /> 
+  </div>
+  
+  
+  </>;
 };
 
 export default Home;
