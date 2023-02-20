f6826659ef8b8caf30a1afdf11cd5f6abc805420
Tildozer <actninswitch@gmail.com>
2023-02-19 17:09:47 -0700

    singleActivtyRoutines works

--- src/components/SingleActivityRoutines.js
+++ src/components/SingleActivityRoutines.js
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getRoutinesByActivity, getUsersPublicRoutines } from "../api";
+import { useLocation } from "react-router-dom";
+import {Routines} from ".";
 
-const SingleActivityRoutines = () => {
-  return <div>HEer</div>;
+const SingleActivityRoutines = (props) => {
+  const {token, user} = props;
+  const [routines, setRoutines] = useState([])
+  const location = useLocation();
+  const pathName = location.pathname;
+  const path = pathName.split("/");
+  const id = pathName.split("/")[path.length - 1];
+
+  const getRoutines = async () => {
+    const activityRoutines = await getRoutinesByActivity({id})
+    console.log(activityRoutines)
+    setRoutines(activityRoutines)
+  }
+  useEffect(() => {
+    getRoutines();
+  }, [id])
+
+  return (
+    <div>
+      <Routines
+        routines={routines}
+        user={user}
+        token={token}
+        callback={getRoutines}
+      />
+    </div>
+    );
 };
 
 export default SingleActivityRoutines;
--- src/components/SingleUserRoutines.js
+++ src/components/SingleUserRoutines.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { getUsersPublicRoutines } from "../api";
 import { useLocation } from "react-router-dom";
-import Routines from "./Routines";
+import {Routines} from ".";
 
 const SingleUserRoutines = (props) => {
   const { token, user } = props;
